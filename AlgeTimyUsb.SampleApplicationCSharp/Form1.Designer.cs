f796065a30a1e0bfbf8900563125ca4e1c875f50
Adam Gajzlerowicz <nelf86@gmail.com>
2025-09-20 17:33:07 +0200

    ..

--- AlgeTimyUsb.SampleApplicationCSharp/Form1.Designer.cs
+++ AlgeTimyUsb.SampleApplicationCSharp/Form1.Designer.cs
@@ -41,13 +41,12 @@
             this.btnClearLog = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
-            // 
+            //
             // button1
-            // 
+            //
             this.button1.Location = new System.Drawing.Point(251, 16);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(141, 23);
@@ -55,9 +54,9 @@
             this.button1.Text = "Send command";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
+            //
             // listBox1
-            // 
+            //
             this.listBox1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 14;
@@ -66,17 +65,17 @@
             this.listBox1.Size = new System.Drawing.Size(391, 200);
             this.listBox1.TabIndex = 1;
             this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
-            // 
+            //
             // txtCommand
-            // 
+            //
             this.txtCommand.Location = new System.Drawing.Point(6, 19);
             this.txtCommand.Name = "txtCommand";
             this.txtCommand.Size = new System.Drawing.Size(239, 20);
             this.txtCommand.TabIndex = 2;
             this.txtCommand.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtCommand_KeyDown);
-            // 
+            //
             // btnStart
-            // 
+            //
             this.btnStart.Location = new System.Drawing.Point(6, 19);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(75, 23);
@@ -84,9 +83,9 @@
             this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.button2_Click);
-            // 
+            //
             // btnStop
-            // 
+            //
             this.btnStop.Location = new System.Drawing.Point(87, 19);
             this.btnStop.Name = "btnStop";
             this.btnStop.Size = new System.Drawing.Size(75, 23);
@@ -94,9 +93,9 @@
             this.btnStop.Text = "Stop";
             this.btnStop.UseVisualStyleBackColor = true;
             this.btnStop.Click += new System.EventHandler(this.button3_Click);
-            // 
+            //
             // button4
-            // 
+            //
             this.button4.Location = new System.Drawing.Point(251, 45);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(141, 23);
@@ -104,18 +103,18 @@
             this.button4.Text = "Send Bytes";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
+            //
             // txtBytes
-            // 
+            //
             this.txtBytes.Location = new System.Drawing.Point(6, 48);
             this.txtBytes.Name = "txtBytes";
             this.txtBytes.Size = new System.Drawing.Size(239, 20);
             this.txtBytes.TabIndex = 6;
             this.txtBytes.Text = "50,52,4F,47,3F,D";
             this.txtBytes.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBytes_KeyUp);
-            // 
+            //
             // chkRaw
-            // 
+            //
             this.chkRaw.AutoSize = true;
             this.chkRaw.Location = new System.Drawing.Point(247, 23);
             this.chkRaw.Name = "chkRaw";
@@ -123,9 +122,9 @@
             this.chkRaw.TabIndex = 8;
             this.chkRaw.Text = "Raw";
             this.chkRaw.UseVisualStyleBackColor = true;
-            // 
+            //
             // chkBytes
-            // 
+            //
             this.chkBytes.AutoSize = true;
             this.chkBytes.Location = new System.Drawing.Point(301, 23);
             this.chkBytes.Name = "chkBytes";
@@ -133,9 +132,9 @@
             this.chkBytes.TabIndex = 9;
             this.chkBytes.Text = "Bytes";
             this.chkBytes.UseVisualStyleBackColor = true;
-            // 
+            //
             // btnClearLog
-            // 
+            //
             this.btnClearLog.Location = new System.Drawing.Point(168, 19);
             this.btnClearLog.Name = "btnClearLog";
             this.btnClearLog.Size = new System.Drawing.Size(75, 23);
@@ -143,52 +142,41 @@
             this.btnClearLog.Text = "Clear Log";
             this.btnClearLog.UseVisualStyleBackColor = true;
             this.btnClearLog.Click += new System.EventHandler(this.btnClearLog_Click);
-            // 
+            //
             // groupBox1
-            // 
+            //
             this.groupBox1.Controls.Add(this.btnStart);
             this.groupBox1.Controls.Add(this.chkBytes);
             this.groupBox1.Controls.Add(this.listBox1);
             this.groupBox1.Controls.Add(this.chkRaw);
             this.groupBox1.Controls.Add(this.btnStop);
             this.groupBox1.Controls.Add(this.btnClearLog);
-            this.groupBox1.Location = new System.Drawing.Point(12, 99);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(403, 253);
             this.groupBox1.TabIndex = 10;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Connection && Logging";
-            // 
+            //
             // groupBox2
-            // 
+            //
             this.groupBox2.Controls.Add(this.button1);
             this.groupBox2.Controls.Add(this.txtCommand);
             this.groupBox2.Controls.Add(this.txtBytes);
             this.groupBox2.Controls.Add(this.button4);
-            this.groupBox2.Location = new System.Drawing.Point(12, 358);
+            this.groupBox2.Location = new System.Drawing.Point(12, 271);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(403, 76);
             this.groupBox2.TabIndex = 11;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Send";
-            // 
-            // label1
-            // 
-            this.label1.BackColor = System.Drawing.Color.Moccasin;
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label1.Location = new System.Drawing.Point(15, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(400, 68);
-            this.label1.TabIndex = 13;
-            this.label1.Text = resources.GetString("label1.Text");
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
+            //
+            //
             // Form1
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(437, 448);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(437, 361);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
@@ -217,7 +205,5 @@
         private System.Windows.Forms.Button btnClearLog;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Label label1;
     }
 }
-
